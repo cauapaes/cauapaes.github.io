f182bc884249571d9ce7e38b8e835843e211455d
cauapaes <cauapaes87@gmail.com>
2025-05-28 12:26:13 -0300

    Menos Por mais

---
+++ Menos Por Mais/script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const input = document.querySelector('.search-input');
+    const listItems = document.querySelectorAll('.produto-item');
+  
+    function normalizeText(text) {
+      return text
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '')
+        .toLowerCase();
+    }
+  
+    // Filtro ao digitar
+    input.addEventListener('input', function () {
+      const searchText = normalizeText(input.value);
+  
+      listItems.forEach(item => {
+        const itemText = normalizeText(item.querySelector('.descricao').textContent);
+        item.style.display = itemText.includes(searchText) ? '' : 'none';
+      });
+    });
+  
+    // Clique em "Comprar Agora" abre link em nova aba/janela
+    listItems.forEach(item => {
+      const link = item.querySelector('.link-produto');
+      const url = item.dataset.url;
+  
+      link.addEventListener('click', function (e) {
+        e.preventDefault();
+        if (url) {
+          window.open(url, '_blank'); // Abre o link em nova aba
+        }
+      });
+    });
+  });
